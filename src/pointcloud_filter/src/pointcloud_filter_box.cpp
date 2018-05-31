b081b3d426df5c4c8bff49b78e3ea1dac7780428
WeiKangLeong <leongweikang@hotmail.com>
2018-05-31 17:51:33 +0800

    add projection cloud

--- src/pointcloud_filter/src/pointcloud_filter_box.cpp
+++ src/pointcloud_filter/src/pointcloud_filter_box.cpp
@@ -14,21 +14,14 @@
 #include <tf_conversions/tf_eigen.h>
 #include <visualization_msgs/MarkerArray.h>
 
-#include "../../../include/grid.h"
-
 #include <math.h>
 
-#define RES 0.5
-#define RANGE_X 800.0
-#define RANGE_Y 800.0
-
-Grid* grid_map_;
-
-bool map_created_;
 
 ros::Publisher pub_floor, pub_static, pub_move;
 
 tf::TransformListener *tf_listener_;
+
+Eigen::Matrix3d intrinsic_;
 
 std::vector<std::vector<int>* >* floor_in_grid_;
 
@@ -39,6 +32,7 @@ pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_left_over(new pcl::PointCloud<pcl::Po
 pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_floor(new pcl::PointCloud<pcl::PointXYZ>);
 pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_floor_dense(new pcl::PointCloud<pcl::PointXYZ>);
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_color(new pcl::PointCloud<pcl::PointXYZRGB>);
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_on_image(new pcl::PointCloud<pcl::PointXYZRGB>);
 
 pcl::PassThrough<pcl::PointXYZI> pass;
 
@@ -77,30 +71,37 @@ void pointcloud_cb(const sensor_msgs::PointCloud2ConstPtr& input)
     std::cout<<"Pointcloud size: "<<cloud_size<<std::endl;
     int prev_group = cloud_color->points[0].r*1000000+cloud_color->points[0].g*1000+cloud_color->points[0].b;
     pcl::PointXYZI point_left;
+    pcl::PointXYZRGB point_project;
     int count=0;
 
     for (int i=0; i<cloud_size; i++)
     {
         int group = cloud_color->points[i].r*1000000+cloud_color->points[i].g*1000+cloud_color->points[i].b;
         //std::cout<<"group: "<<int(cloud_color->points[i].r*1000000+cloud_color->points[i].g*1000+cloud_color->points[i].b)<<std::endl;
+        point_left.x=cloud_color->points[i].x;
+        point_left.y=cloud_color->points[i].y;
+        point_left.z=cloud_color->points[i].z;
         if (group==prev_group)
         {
-            point_left.x=cloud_color->points[i].x;
-            point_left.y=cloud_color->points[i].y;
-            point_left.z=cloud_color->points[i].z;
-            point_left.intensity=count;
-            cloud_left_over->push_back(point_left);
+
+
         }
         else
         {
             count++;
             prev_group=group;
-            point_left.x=cloud_color->points[i].x;
-            point_left.y=cloud_color->points[i].y;
-            point_left.z=cloud_color->points[i].z;
-            point_left.intensity=count;
-            cloud_left_over->push_back(point_left);
+
         }
+        point_left.intensity=count;
+        cloud_left_over->push_back(point_left);
+        point_project.x = point_left.x;
+        point_project.y = point_left.y;
+        point_project.z = point_left.z;
+
+        point_project.r = cloud_color->points[i].r;
+        point_project.g = cloud_color->points[i].g;
+        point_project.b = cloud_color->points[i].b;
+        cloud_on_image->push_back(point_project);
     }
 
     std::cout<<"Total cluster: "<<count<<std::endl;
@@ -120,10 +121,6 @@ int main (int argc, char** argv)
     ros::init (argc, argv, "pointcloud_filter_box");
     ros::NodeHandle nh;
 
-    grid_map_ = new Grid(RANGE_X, RANGE_Y, 0, RES, 0, 0);
-
-    map_created_=false;
-
     tf_listener_ = new tf::TransformListener();
 
     //ros::Subscriber sub_box = nh.subscribe<visualization_msgs::MarkerArray> ("/percept_info", 1, box_cb);
@@ -133,6 +130,8 @@ int main (int argc, char** argv)
     pub_floor = nh.advertise<sensor_msgs::PointCloud2> ("/floor_points", 1);
     pub_static = nh.advertise<sensor_msgs::PointCloud2> ("/cloud_out", 1);
     //pub_cluster = nh.advertise<sensor_msgs::PointCloud2> ("/cloud_left", 1);
+
+    intrinsic_ << 484.538, 0.0, 371.507, 0.0, 484.798, 184.8, 0.0, 0.0, 1.0;
 
 
     ros::spin ();
