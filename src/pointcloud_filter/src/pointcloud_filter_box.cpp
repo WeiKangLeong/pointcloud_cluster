bc35a9a64fe26831381b8abd5a3d48da0e2bc734
WeiKangLeong <leongweikang@hotmail.com>
2018-06-05 21:39:47 +0800

    pointcloud to 2d

--- src/pointcloud_filter/src/pointcloud_filter_box.cpp
+++ src/pointcloud_filter/src/pointcloud_filter_box.cpp
@@ -27,14 +27,14 @@ std::vector<std::vector<int>* >* floor_in_grid_;
 
 pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_comeout(new pcl::PointCloud<pcl::PointXYZI>);
 pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_in(new pcl::PointCloud<pcl::PointXYZI>);
-pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZI>);
+//pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZI>);
 pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_left_over(new pcl::PointCloud<pcl::PointXYZI>);
 pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_floor(new pcl::PointCloud<pcl::PointXYZ>);
 pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_floor_dense(new pcl::PointCloud<pcl::PointXYZ>);
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_color(new pcl::PointCloud<pcl::PointXYZRGB>);
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_on_image(new pcl::PointCloud<pcl::PointXYZRGB>);
 
-pcl::PassThrough<pcl::PointXYZI> pass;
+pcl::PassThrough<pcl::PointXYZRGB> pass;
 
 /*void box_cb(const visualization_msgs::MarkerArrayConstPtr& markers_array)
 {
@@ -61,14 +61,24 @@ void pointcloud_cb(const sensor_msgs::PointCloud2ConstPtr& input)
 
     int cloud_size = cloud_color->size();
 
-//    pass.setInputCloud (cloud_comeout);
-//    pass.setFilterFieldName ("x");
-//    pass.setFilterLimits (-40.0, 40.0);
-//    pass.filter (*cloud_filtered);
-//    pass.setFilterFieldName ("y");
-//    pass.setFilterLimits (-25.0, 25.0);
-//    pass.filter (*cloud_filtered);
-    std::cout<<"Pointcloud size: "<<cloud_size<<std::endl;
+//    x:0.85
+//    y:-+0.7
+//    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZRGB>);
+    //cloud_comeout = cloud_color;
+    pass.setInputCloud (cloud_color);
+    pass.setFilterFieldName ("x");
+    pass.setFilterLimits (0.85, 200.0);
+    pass.filter (*cloud_color);
+    pass.setFilterFieldName ("y");
+    pass.setFilterLimits (-25.0, 25.0);
+    pass.filter (*cloud_color);
+
+    sensor_msgs::PointCloud2 output3;
+    pcl::toROSMsg(*cloud_color, output3);
+    output3.header = input->header;
+    pub_static.publish(output3);
+
+    std::cout<<"Pointcloud size: "<<cloud_size<<" to: "<<cloud_color->size()<<std::endl;
     int prev_group = cloud_color->points[0].r*1000000+cloud_color->points[0].g*1000+cloud_color->points[0].b;
     pcl::PointXYZI point_left;
     pcl::PointXYZRGB point_project;
@@ -80,9 +90,9 @@ void pointcloud_cb(const sensor_msgs::PointCloud2ConstPtr& input)
     {
         int group = cloud_color->points[i].r*1000000+cloud_color->points[i].g*1000+cloud_color->points[i].b;
         //std::cout<<"group: "<<int(cloud_color->points[i].r*1000000+cloud_color->points[i].g*1000+cloud_color->points[i].b)<<std::endl;
-        point_left.x=cloud_color->points[i].x;
-        point_left.y=cloud_color->points[i].y;
-        point_left.z=cloud_color->points[i].z;
+        point_left.x=-cloud_color->points[i].y;
+        point_left.y=cloud_color->points[i].z;
+        point_left.z=cloud_color->points[i].x;
 
         Eigen::Vector3d point_vector(point_left.x, point_left.y, point_left.z);
         //after_project = point_vector.cross(intrinsic_);
@@ -100,9 +110,13 @@ void pointcloud_cb(const sensor_msgs::PointCloud2ConstPtr& input)
         }
         point_left.intensity=count;
         cloud_left_over->push_back(point_left);
-        point_project.x = point_vector.dot(intrinsic_1_);
-        point_project.y = point_vector.dot(intrinsic_2_);
-        point_project.z = point_vector.dot(intrinsic_3_);;
+        point_project.z = point_vector.dot(intrinsic_3_);
+        point_project.x = point_vector.dot(intrinsic_1_)/point_project.z;
+        point_project.y = point_vector.dot(intrinsic_2_)/point_project.z;
+        point_project.z = point_project.z/point_project.z;
+
+
+
 
         point_project.r = cloud_color->points[i].r;
         point_project.g = cloud_color->points[i].g;
@@ -112,9 +126,12 @@ void pointcloud_cb(const sensor_msgs::PointCloud2ConstPtr& input)
 
     std::cout<<"Total cluster: "<<count<<std::endl;
     sensor_msgs::PointCloud2 output2;
-    pcl::toROSMsg(*cloud_left_over, output2);
+    pcl::toROSMsg(*cloud_on_image, output2);
     output2.header = input->header;
     pub_move.publish(output2);
+
+    cloud_on_image->clear();
+    cloud_left_over->clear();
 
     std::cout<<"time taken: "<<time.toc()<<std::endl;
 
