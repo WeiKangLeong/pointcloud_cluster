d816200bcdb5da10675760b45339a5517e0b6506
WeiKangLeong <35618798+WeiKangLeong@users.noreply.github.com>
2019-03-12 16:01:56 +0800

    Update pointcloud_filter_vertical_feature.cpp

--- src/pointcloud_filter/src/pointcloud_filter_vertical_feature.cpp
+++ src/pointcloud_filter/src/pointcloud_filter_vertical_feature.cpp
@@ -1,4 +1,4 @@
-﻿#include <ros/ros.h>
+#include <ros/ros.h>
 // PCL specific includes
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
@@ -7,7 +7,6 @@
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
-#include <pcl/filters/extract_indices.h>
 
 #include <tf/transform_listener.h>
 #include <pcl/kdtree/kdtree_flann.h>
